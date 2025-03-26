9a518a18b3b028426dc7ff5b5aa18152fd010a5a
xyryc <amdanikzz@gmail.com>
2025-03-26 18:10:08 +0600

    added pricing section

---
+++ src/components/sections/Pricing.jsx
@@ -0,0 +1,241 @@
+import { useEffect } from "react";
+import Button from "../Button";
+import { motion, useAnimation } from "motion/react";
+import { useInView } from "react-intersection-observer";
+
+const logoVariants = {
+  hidden: { opacity: 0, y: 30 },
+  visible: { opacity: 1, y: 0, transition: { duration: 0.6 } },
+};
+
+const Pricing = () => {
+  const controls = useAnimation();
+  const { ref, inView } = useInView({ triggerOnce: false, threshold: 0.3 });
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    } else {
+      controls.start("hidden");
+    }
+  }, [controls, inView]);
+
+  return (
+    <section className="py-10 bg-[#020617] sm:py-16 lg:py-24" ref={ref}>
+      <div className="max-w-5xl px-4 mx-auto sm:px-6 lg:px-8">
+        <motion.div
+          className="grid grid-cols-1 md:items-stretch md:grid-cols-2 gap-x-12 lg:gap-x-20 gap-y-10"
+          initial="hidden"
+          animate={controls}
+          variants={logoVariants}
+        >
+          <div className="flex flex-col justify-between lg:py-5">
+            <h2 className="text-3xl font-bold leading-tight text-white sm:text-4xl lg:leading-tight lg:text-5xl">
+              Join 35k+ Satisfied Clients & Achieve Your Financial Goals
+            </h2>
+
+            <div className="mt-auto">
+              <div className="flex items-center">
+                <svg
+                  className="w-6 h-6 text-orange-500"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                </svg>
+                <svg
+                  className="w-6 h-6 text-orange-500"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                </svg>
+                <svg
+                  className="w-6 h-6 text-orange-500"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                </svg>
+                <svg
+                  className="w-6 h-6 text-orange-500"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                </svg>
+                <svg
+                  className="w-6 h-6 text-orange-500"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                </svg>
+              </div>
+
+              <blockquote className="mt-6">
+                <p className="text-lg leading-relaxed text-white">
+                  Getting the loan I needed was quick and hassle-free. The
+                  application process was straightforward, and I had the funds
+                  in my account in no time. Highly recommend for anyone seeking
+                  financial support.
+                </p>
+              </blockquote>
+
+              <div className="flex items-center mt-8">
+                <img
+                  className="flex-shrink-0 object-cover w-10 h-10 rounded-full"
+                  src="https://cdn.rareblocks.xyz/collection/celebration/images/pricing/2/avatar.jpg"
+                  alt=""
+                />
+                <div className="ml-4">
+                  <p className="text-base font-semibold text-white">
+                    Brooklyn Simmons
+                  </p>
+                  <p className="mt-px text-sm text-gray-400">
+                    Small Business Owner
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <div className="overflow-hidden bg-white rounded-md">
+              <div className="p-10">
+                <h3 className="text-xs font-semibold tracking-widest text-purple-600 uppercase">
+                  Personal Loan Package
+                </h3>
+                <p className="mt-4 text-6xl font-bold text-black">$79</p>
+
+                <ul className="flex flex-col mt-8 space-y-4">
+                  <li className="inline-flex items-center space-x-2">
+                    <svg
+                      className="flex-shrink-0 w-5 h-5 text-green-500"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                    <span className="text-base font-medium text-gray-900">
+                      For Personal & Business Loans
+                    </span>
+                  </li>
+
+                  <li className="inline-flex items-center space-x-2">
+                    <svg
+                      className="flex-shrink-0 w-5 h-5 text-green-500"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                    <span className="text-base font-medium text-gray-900">
+                      Access to Competitive Rates
+                    </span>
+                  </li>
+
+                  <li className="inline-flex items-center space-x-2">
+                    <svg
+                      className="flex-shrink-0 w-5 h-5 text-green-500"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                    <span className="text-base font-medium text-gray-900">
+                      Easy Online Application
+                    </span>
+                  </li>
+
+                  <li className="inline-flex items-center space-x-2">
+                    <svg
+                      className="flex-shrink-0 w-5 h-5 text-green-500"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                    <span className="text-base font-medium text-gray-900">
+                      Fast Approval Process
+                    </span>
+                  </li>
+
+                  <li className="inline-flex items-center space-x-2">
+                    <svg
+                      className="flex-shrink-0 w-5 h-5 text-green-500"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                    <span className="pb-0.5 text-base font-medium text-gray-900 border-b border-black border-dashed">
+                      Secure & Confidential
+                    </span>
+                  </li>
+                </ul>
+
+                <div className="py-4 mt-10 *:w-full">
+                  <Button text={"Get Started Now"} />
+                </div>
+
+                <div className="flex items-center mt-5">
+                  <svg
+                    className="flex-shrink-0 w-5 h-5 text-gray-500"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
+                    />
+                  </svg>
+                  <span className="ml-2 text-sm text-gray-500">
+                    {" "}
+                    14 Days Moneyback Guarantee{" "}
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+export default Pricing;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Hero from "../components/sections/Hero";
 import LogoCloud from "../components/sections/LogoCloud";
+import Pricing from "../components/sections/Pricing";
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <Pricing />
       <LogoCloud />
     </>
   );
